3c748931d1b315f2668781be0a7e580907501745
Florent Beauchamp <flo850@free.fr>
2022-01-21 16:22:20 +0100

    feat(xo-server): implement authorization mixin

---
+++ packages/xo-server/src/xo-mixins/authorization.mjs
@@ -0,0 +1,41 @@
+
+import { createLogger } from '@xen-orchestra/log'
+import  get from 'lodash/get.js';
+
+const log = createLogger('xo:store')
+
+class UnauthorizedFeatureError extends Error{
+  constructor(featureCode) {
+    super(`feature ${featureCode} is not authorized for current installation`);
+    this.name = "UnauthorizedFeatureError";
+    this.featureCode = featureCode;
+  }
+}
+
+const AUTHORIZATIONS = {
+  BACKUP: {
+    FULL: 2,
+    METADATA: 3,
+    REPORT: 2,
+    S3: 2
+  },
+  PLUGIN: {
+    BACKUP: {
+      REPORTS: 3
+    }
+  }
+}
+
+
+export default class {
+
+  _getMinPlan(featureCode){
+    return get(AUTHORIZATIONS, featureCode, 0)
+  }
+
+  checkFeatureAuthorization(featureCode){
+    if(process.env.XOA_PLAN < this._getMinPlan(featureCode)){
+      throw new UnauthorizedFeatureError(featureCode)
+    }
+  }
+}
